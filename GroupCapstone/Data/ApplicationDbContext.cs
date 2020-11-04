3fc8137589d2a7e195b8a9725b91f66aa42ef9e2
pttiming <sean@pttiming.com>
2020-11-04 15:44:38 -0600

    minor tweaks to get pusher chat running

--- GroupCapstone/Data/ApplicationDbContext.cs
+++ GroupCapstone/Data/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ namespace GroupCapstone.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public DbSet<User> Users { get; set; } 
+        public DbSet<Participant> Participant { get; set; } 
 
         public DbSet<Event> Event { get; set; }
 
