3239506cf528f423b7b0eb57e66cda18d893fb54
pttiming <sean@pttiming.com>
2020-11-04 14:30:05 -0600

    fixed gitignore issue

--- GroupCapstone/API_KEYS.cs
+++ GroupCapstone/API_KEYS.cs
@@ -8,10 +8,10 @@ namespace GroupCapstone
 {
     public static class API_KEYS
     {
-        public static string googleMapsApi = "AIzaSyDvZ26-BeT5UBopkfDheEW-9TsPCNY6yrE";
+        public static string googleMapsApi = "";
 
-        public static string pusherAPI = "ed1f63a5d01a8b4e629c";
+        public static string pusherAPI = "";
 
-        public static string yelpAPI = "z5I-OAHRfD_y5N7eEOSHlZ7VcGMS9nv-dNK-po9vIGgYUlrisa9jyFIhmawgQbAogEpWe2rROAjmFY52TpFsX7Qw6UHpN8liIfXaD0OQb_o_mcduuifyOFl9FJmhX3Yx";        
+        public static string yelpAPI = "";        
     }
 }
